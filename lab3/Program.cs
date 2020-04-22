4779467af23a7d197094f5ed9955199754d3d646
bohdan.fedorchenko <bogdan.ferdorchenko@tatemgames.com>
2020-04-22 21:40:43 +0300

    Add console app and make simple program flow

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace lab3
 {
+    using AppContext;
+    using ConsoleApp;
+
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var context = new AppContext();
+            context.Start();
+            var program = new ConsoleProgram();
+            program.Init();
+            program.ProcessInput();
+            program.Exit();
         }
     }
 }
